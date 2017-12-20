2823ef771208171ebb85f24fcb03c2aeb46763f1
JWWon <wonjiwn@naver.com>
2017-12-20 16:55:23 +0900

    set android & ios fbsdk done

--- android/app/src/main/java/com/livle_client/MainApplication.java
+++ android/app/src/main/java/com/livle_client/MainApplication.java
@@ -11,10 +11,19 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -26,7 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
+            new FBSDKPackage(mCallbackManager),
             new ReactNativeYouTube(),
             new ReactNativeLocalizationPackage()
       );
