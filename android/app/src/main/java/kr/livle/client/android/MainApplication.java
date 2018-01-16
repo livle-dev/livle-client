6b8a78d96c62379436796a6bcc8ede9a6dec7861
JWWon <wonjiwn@naver.com>
2018-01-16 22:18:30 +0900

    update react-native & npm, install google-analytics-bridge

--- android/app/src/main/java/kr/livle/client/android/MainApplication.java
+++ android/app/src/main/java/kr/livle/client/android/MainApplication.java
@@ -3,6 +3,7 @@ package kr.livle.client.android;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new GoogleAnalyticsBridgePackage(),
             new FIRMessagingPackage(),
             new ReactVideoPackage(),
             new LinearGradientPackage(),
